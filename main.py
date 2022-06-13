17752f285c665ec3afcf28b15621e242539bbd4f
Khutso <khutsofrans4@gmail.com>
2022-06-13 21:03:02 +0200

    added anime finding capability

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import secrets
+from typing import Match
 import pandas as pd
 import requests
-
-#match = False
+import json
+import pprint
+import time
 
 res = requests.get('https://api.jikan.moe/v4/genres/anime')
+match = True
 genre_list = []
+
 for i in range(0, 76):
     genre_list.append(str(res.json()['data'][i]['name']))
 
@@ -13,39 +17,75 @@ col = ['GENRES']
 df = pd.DataFrame(genre_list, columns=col)
 pd.set_option('display.max_rows', None)
 
-
 print('Welcome to anime recommender!')
-type = input('Anime type: [TV / Movie]? ').lower()
+type = input('Anime type: [TV / Movie / OVA / Special]? ').lower()
 print(df)
 mood = input('Genre? ').lower()
 
-i = secrets.randbelow(2000)
-response = requests.get(f'https://api.jikan.moe/v4/anime/{i}')
 
-anime_type = str(response.json()['data']['type']).lower()
-try:
-    anime_genre1 = str(response.json()['data']['genres'][0]['name']).lower()
-    anime_genre2 = str(response.json()['data']['genres'][1]['name']).lower()
-    anime_genre3 = str(response.json()['data']['genres'][2]['name']).lower()
-except IndexError:
-    pass
+def find_anime(type, mood):
+    match = False
+    i = secrets.randbelow(2000)
+    response = requests.get(f'https://api.jikan.moe/v4/anime/{i}')
+    # pprint.pprint(response.json())
+    try:
+        anime_type = str(response.json()['data']['type']).lower()
+    except KeyError:
+        time.sleep(10)
+        response = requests.get(f'https://api.jikan.moe/v4/anime/{1}')
+        anime_type = str(response.json()['data']['type']).lower()
+    try:
+        anime_genre1 = str(
+            response.json()['data']['genres'][0]['name']).lower()
+    except IndexError:
+        pass
+    try:
+        anime_genre2 = str(
+            response.json()['data']['genres'][1]['name']).lower()
+    except IndexError:
+        anime_genre2 = str(
+            response.json()['data']['genres'][0]['name']).lower()
+    try:
+        anime_genre3 = str(
+            response.json()['data']['genres'][2]['name']).lower()
+    except IndexError:
+        anime_genre3 = str(
+            response.json()['data']['genres'][0]['name']).lower()
 
-anime_title = response.json()['data']['title']
-anime_synopsis = response.json()['data']['synopsis']
-if anime_type == type and anime_genre1 == mood:
-    print('\nYour anime recommendation is: \n')
-    print(anime_title)
-    print(anime_synopsis)
-    #match = True
-elif anime_type == type and anime_genre2 == mood:
-    print(anime_title)
-    print(anime_synopsis)
-elif anime_type == type and anime_genre3 == mood:
-    print(anime_title)
-    print(anime_synopsis)
-else:
-    print('No match!')
-    print(anime_type)
-    print(anime_genre1)
-    print(anime_title)
-    print(anime_synopsis)
+    anime_title = response.json().get('data', {}).get('title', {})
+    anime_rating = response.json().get('data', {}).get('rating', {})
+    anime_score = response.json().get('data', {}).get('score', {})
+    anime_synopsis = response.json().get('data', {}).get('synopsis', {})
+
+    if anime_type == type and anime_genre1 == mood:
+        print('\nYour anime recommendation is: \n')
+        print(anime_title)
+        print(anime_rating)
+        print(f'{anime_score}/10')
+        print(anime_synopsis)
+        match = True
+    elif anime_type == type and anime_genre2 == mood:
+        print('\nYour anime recommendation is: \n')
+        print(anime_title)
+        print(anime_rating)
+        print(f'{anime_score}/10')
+        print(anime_synopsis)
+        match = True
+    elif anime_type == type and anime_genre3 == mood:
+        print('\nYour anime recommendation is: \n')
+        print(anime_title)
+        print(anime_rating)
+        print(f'{anime_score}/10')
+        print(anime_synopsis)
+        match = True
+    else:
+        print('No match!')
+
+    return match
+
+
+timeout = 60
+timeout_start = time.time()
+while(time.time() < timeout_start + timeout):
+    if find_anime(type, mood) == match:
+        break
